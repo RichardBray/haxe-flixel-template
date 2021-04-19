f8be6f4a5fa87d6fc54e9113d73770f7d0c99ccf
Richard Bray <richardbray89@gmail.com>
2021-04-19 15:37:44 +0100

    chore: comment out default preloader
    And set width for debugging on the web

--- src/Main.hx
+++ src/Main.hx
@@ -1,12 +1,14 @@
 package;
 
 import flixel.FlxGame;
+
 import openfl.display.Sprite;
+
 import states.GameState;
 
 class Main extends Sprite {
 	public function new() {
 		super();
-		addChild(new FlxGame(0, 0, GameState));
+		addChild(new FlxGame(1920, 1080, GameState, true));
 	}
 }
