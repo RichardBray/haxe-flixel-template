8d2c04a2c19407d8baf6c393d839d1ba4135901d
Richard Bray <richardbray89@gmail.com>
2021-04-19 15:35:53 +0100

    chore: update file structure and formatting
    As well as compilation server port

--- watcher.sh
+++ watcher.sh
@@ -4,7 +4,7 @@ GREEN='\033[0;32m'
 NC="\033[0m" # No Color
 
 echo "🔨 Building game!!"
-npx lix lime build html5 -debug --connect 6000
+lix lime build html5 -debug --connect 7000
 # osascript is Mac specific
 osascript -e 'display notification "🎉 Build finished!!!" with title "HaxeFlixel" sound name "Ping"'
 echo "${GREEN}🎉 Buidling finished!!!${NC}"
