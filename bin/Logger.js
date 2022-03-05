6f0b1bcf49b309b134351e480cf6203a83908f84
Richard Oliver Bray <richardbray89@gmail.com>
2022-03-05 19:49:23 +0000

    refactor: add log to logger and remove color vars

--- bin/Logger.js
+++ bin/Logger.js
@@ -1,28 +1,31 @@
-const colors = {
-  red: '\x1b[31m%s\x1b[0m',
-  yellow: '\x1b[33m%s\x1b[0m',
-  cyan: '\x1b[36m%s\x1b[0m',
-  green: '\x1b[32m%s\x1b[0m'
-};
-
-const LCERROR = colors.red;
-const LCWARN = colors.yellow;
-const LCINFO = colors.cyan;
-const LCSUCCESS = colors.green;
-
+// @ts-check
 class Logger {
+  static colors = {
+    red: '\x1b[31m%s\x1b[0m',
+    yellow: '\x1b[33m%s\x1b[0m',
+    cyan: '\x1b[36m%s\x1b[0m',
+    green: '\x1b[32m%s\x1b[0m',
+  };
+
   static error(message, ...optionalParams) {
-    console.error(LCERROR, message, ...optionalParams);
+    console.error(Logger.colors.red, message, ...optionalParams);
   }
+
   static warn(message, ...optionalParams) {
-    console.warn(LCWARN, message, ...optionalParams);
+    console.warn(Logger.colors.yellow, message, ...optionalParams);
   }
+
   static info(message, ...optionalParams) {
+    console.info(Logger.colors.cyan, message, ...optionalParams);
+  }
+
+  static success(message, ...optionalParams) {
+    console.info(Logger.colors.green, message, ...optionalParams);
+  }
+
+  static log(message, ...optionalParams) {
     console.info(message, ...optionalParams);
   }
-  static success(message, ...optionalParams) {
-    console.info(LCSUCCESS, message, ...optionalParams);
-  }
-};
+}
 
 export default Logger;
